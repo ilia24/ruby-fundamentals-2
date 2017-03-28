2332daf292487974cb8233132c60874a80ac1096
ilia24 <iliademertchian24@gmail.com>
2017-03-28 14:57:02 -0400

    Complete exercise 9

--- exercise9.rb
+++ exercise9.rb
@@ -1,4 +1,4 @@
-grocery_list = ["chicken", "mustard", "frozenmango", "dietcoke"]
+grocery_list = ["chicken", "mustard", "frozenmango", "dietcoke", "🐔", "salmon"]
 
 grocery_list.each do |item|
   puts "* #{item}"
@@ -7,5 +7,27 @@ end
 grocery_list << "rice"
 
 grocery_list.each do |item|
+  puts "* #{item}"
+end
+
+def groceryadd(piece, array)
+  array << piece
+  array.each do |item|
+    puts "* #{item}"
+  end
+end
+
+groceryadd("thingy", grocery_list)
+
+puts grocery_list.length
+puts grocery_list[1]
+
+grocery_list.sort.each do |item|
+  puts "* #{item}"
+end
+
+grocery_list.delete("salmon")
+
+grocery_list.sort.each do |item|
   puts "* #{item}"
 end
